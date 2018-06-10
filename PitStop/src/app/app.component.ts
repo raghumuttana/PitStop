86b069a3bfcb26941788e00758baf3b82909c4e6
Raghu Reddy <r.reddy@outlook.com>
2018-06-10 18:04:27 -0400

    first checkin after angular cli template created

---
+++ PitStop/src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component(
+  {
+    selector: 'app-root',
+    templateUrl: './app.component.html'
+  }
+)
+export class AppComponent {
+  pageTitle = 'Pit Stop for System Administrators';
+}
+
