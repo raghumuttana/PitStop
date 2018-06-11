74af48575d822f05810ae83d0cfbfa35e5421e7e
Raghu Reddy <r.reddy@outlook.com>
2018-06-10 21:06:57 -0400

    offering-list component related work

---
+++ PitStop/src/offerings/offering-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-offerings',
+    templateUrl: './offering-list.component.html'
+})
+
+export class OfferingListComponent implements OnInit {
+    constructor() { }
+
+    ngOnInit() { }
+}
