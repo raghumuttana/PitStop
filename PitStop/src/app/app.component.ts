f7583dedcdf1a0f961d7adf28ea036cfe2615073
Raghu Reddy <r.reddy@outlook.com>
2018-06-23 00:54:11 -0400

    roles

--- PitStop/src/app/app.component.ts
+++ PitStop/src/app/app.component.ts
@@ -25,45 +25,26 @@ export class AppComponent implements OnInit {
 
 getBooks(): TreeviewItem[] {
   const childrenCategory = new TreeviewItem({
-      text: 'Children', value: 1, collapsed: true, children: [
-          { text: 'Baby 3-5', value: 11 },
-          { text: 'Baby 6-8', value: 12 },
-          { text: 'Baby 9-12', value: 13 }
+      text: 'Server Role', value: 1, checked: false, collapsed: false, children: [
+          { text: 'Raw Disk Configuration', checked: false, value: 11 },
+          { text: 'Clustering', checked: false, value: 12, children :
+                        [{text: 'Add Node to Cluster', checked: false, value: 13},
+                         {text: 'Create a Cluster', checked: false, value: 14}]}
       ]
   });
   const itCategory = new TreeviewItem({
-      text: 'IT', value: 9, children: [
-          {
-              text: 'Programming', value: 91, children: [{
-                  text: 'Frontend', value: 911, children: [
-                      { text: 'Angular 1', value: 9111 },
-                      { text: 'Angular 2', value: 9112 },
-                      { text: 'ReactJS', value: 9113, disabled: true }
-                  ]
-              }, {
-                  text: 'Backend', value: 912, children: [
-                      { text: 'C#', value: 9121 },
-                      { text: 'Java', value: 9122 },
-                      { text: 'Python', value: 9123, checked: false, disabled: true }
-                  ]
-              }]
-          },
-          {
-              text: 'Networking', value: 92, children: [
-                  { text: 'Internet', value: 921 },
-                  { text: 'Security', value: 922 }
-              ]
-          }
-      ]
+    text: 'DSC Configuration', value: 2, checked: false, collapsed: false, children: [
+        { text: 'Upload DSC', checked: false, value: 22 },
+        { text: 'Create New DSC', checked: false, value: 12 }
+    ]
   });
-  const teenCategory = new TreeviewItem({
-      text: 'Teen', value: 2, collapsed: true, disabled: true, children: [
-          { text: 'Adventure', value: 21 },
-          { text: 'Science', value: 22 }
-      ]
-  });
-  const othersCategory = new TreeviewItem({ text: 'Others', value: 3, checked: false, disabled: true });
-  return [childrenCategory, itCategory, teenCategory, othersCategory];
+//   const teenCategory = new TreeviewItem({
+//       text: 'Teen', value: 2, collapsed: true, disabled: true, children: [
+//           { text: 'Adventure', value: 21 },
+//           { text: 'Science', value: 22 }
+//       ]
+//   });
+  return [childrenCategory, itCategory];
 }
 
 }
